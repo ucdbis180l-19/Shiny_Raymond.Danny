ed6d20d9b5c0f8efcfe6387e6d87af06fdfa2bb0
Ubuntu <ubuntu@ip-172-31-42-227.ec2.internal>
2019-05-09 23:37:11 +0000

    Added color to the graph

--- webapp/server.R
+++ webapp/server.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 library(tidyverse)
-tomato <- read_csv("/home/ubuntu/Shiny_Raymond.Danny/webapp/Tomato.csv")
+tomato <- read_csv("/home/ubuntu/Assignments/Shiny/webapp/Tomato.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -17,7 +17,7 @@ shinyServer(function(input, output) {
   output$tomato_histo <- renderPlot({
     tomato$pl <- rowSums(tomato[,input$traits])
     
-    ggplot(data = tomato, aes_string(x=tomato$pl)) + geom_histogram() + facet_wrap(~species) + ylab("Count") + ggtitle("Total length by species")
+    ggplot(data = tomato, aes_string(x=tomato$pl,colour="species",fill="species")) + geom_histogram() + facet_wrap(~species) + ylab("Count") + ggtitle("Total length by species")
   })
   
 })
