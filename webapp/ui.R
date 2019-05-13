2e132da2ae3d3bb4b5750544971f85e15bb115e5
Ubuntu <ubuntu@ip-172-31-42-227.ec2.internal>
2019-05-13 18:21:08 +0000

    Made the UI more informative and added x label

--- webapp/ui.R
+++ webapp/ui.R
@@ -14,6 +14,10 @@ shinyUI(fluidPage(
   
   # Application title
   titlePanel("Stem Heights of different Tomato species"),
+  
+  helpText("This application creates a histogram to show the difference between",
+           "the heights of the stem of 5 tomato species.  Please use the check box below to choose any combinations of",
+           "of the various parts of the stem for plotting. The hypocotyl length refers to the height of the stem before the first node, and the internodes are the height between nodes."),
   # Copy the chunk below to make a group of checkboxes
   checkboxGroupInput("traits", label = h3("Stem lengths"), 
                      choices = list("Hypocotyl Length" = "hyp", "Internode 1" = "int1", "Internode 2" = "int2", 
